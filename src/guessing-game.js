eee96d222bdf1885b22c03acbe1ef8fb3f92fbd3
Anna-Mamedova <mamedi289@gmail.com>
2023-02-26 18:59:46 +0200

    feat: add guessing functional

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result
+        this.max
+        this.min
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+        this.result = (this.max - this.min)/2
     }
 
     guess() {
-
+        this.result = (this.max - this.min)/2
+        return Math.round(this.min + this.result)
     }
 
     lower() {
-
+       this.max = Math.round(this.max - this.result)
     }
 
     greater() {
-
+       this.min = Math.round(this.min + this.result)
     }
 }
 
